eac3e2d2f5c15e3f3240239023ed326859d73671
AghastyGD <augustodomingosvasco@gmail.com>
2024-07-03 14:16:59 +0200

    register interviews model in admin panel

--- interviews/admin.py
+++ interviews/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Chat, Message
 
-# Register your models here.
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("title", "job", "completed")
