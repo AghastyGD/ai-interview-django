3bf3718e8ccdd2a1c33e4d9ce9c781f54993b0bd
AghastyGD <augustodomingosvasco@gmail.com>
2024-07-03 14:22:09 +0200

    feat: create message inline

--- interviews/admin.py
+++ interviews/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Chat, Message
 
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 0
+
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
     list_display = ("title", "job", "completed")
+    inlines = (MessageInline,)
