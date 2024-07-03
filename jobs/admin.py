6bcdcb85b343ef3ecd0edd544a930841b1dd434e
AghastyGD <augustodomingosvasco@gmail.com>
2024-07-03 13:52:23 +0200

    feat: admin customization

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Job, Skill
 
-# Register your models here.
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("title", "level", "get_skills")
+    search_fields = ("title", "level")
+    list_filter = ("level", "skills")
+    
+    @admin.display(description="Skills")
+    def get_skills(self, obj):
+        return ", ".join([skill.title for skill in obj.skills.all()])
+
+@admin.register(Skill)
+class SkillsAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title", )
